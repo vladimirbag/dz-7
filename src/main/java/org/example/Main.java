05c3bd390a0788a8eb1ffe645968f2a62a01340e
Volodymyr <52545758+vladimirbag@users.noreply.github.com>
2023-08-02 11:00:54 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,72 +12,64 @@ public class Main {
         System.out.println("Next planet: " + earth.getNextPlanet());
     }
 
-    enum SolarSystem {
-        MERCURY(0, 0, 2439.7, null),
-        VENUS(2, 108.2, 6051.8, MERCURY),
-        EARTH(3, 47.5, 6371.0, VENUS),
-        MARS(4, 52.9, 3389.5, EARTH),
-        JUPITER(5, 228.0, 69911, MARS),
-        SATURN(6, 433.0, 58232, JUPITER),
-        URANUS(7, 579.0, 25362, SATURN),
-        NEPTUNE(8, 449.8, 24622, URANUS),
-        PLUTO(9, 5906.4, 1188.3, NEPTUNE);
+   enum SolarSystem {
+    MERCURY(0, 2439, null),
+    VENUS(57_910_000, 6051, MERCURY),
+    EARTH(108_200_000, 6371, VENUS),
+    MARS(227_940_000, 3389, EARTH),
+    JUPITER(778_330_000, 69_911, MARS),
+    SATURN(1_426_610_000, 58_232, JUPITER),
+    URANUS(2_870_990_000, 25_362, SATURN),
+    NEPTUNE(4_498_250_000, 24_622, URANUS);
 
-        private final int ordinalFromSun;
-        private final double distanceFromPrevious;
-        private final double distanceFromSun;
-        private final double radius;
-        private final SolarSystem previousPlanet;
-        private final SolarSystem nextPlanet;
+    private int distanceFromSun;
+    private int distanceFromPreviousPlanet;
+    private int radius;
+    private SolarSystem previousPlanet;
+    private SolarSystem nextPlanet;
 
-        SolarSystem(int ordinalFromSun, double distanceFromPrevious, double radius, SolarSystem previousPlanet) {
-            this.ordinalFromSun = ordinalFromSun;
-            this.distanceFromPrevious = distanceFromPrevious;
-            this.radius = radius;
-            this.previousPlanet = previousPlanet;
-            this.nextPlanet = getNextPlanet(ordinalFromSun);
-            this.distanceFromSun = calculateDistanceFromSun();
+    SolarSystem(int prevDistance, int radius, SolarSystem previous) {
+        this.distanceFromPreviousPlanet = prevDistance;
+        this.radius = radius;
+        this.previousPlanet = previous;
+
+        if (previous != null) {
+            previous.nextPlanet = this;
+            this.distanceFromSun = previous.distanceFromSun + prevDistance;
+        } else {
+            this.distanceFromSun = 0;
         }
+    }
 
-        public int getOrdinalFromSun() {
-            return ordinalFromSun;
-        }
+    public int getDistanceFromSun() {
+        return distanceFromSun;
+    }
 
-        public double getDistanceFromPrevious() {
-            return distanceFromPrevious;
-        }
+    public int getDistanceFromPreviousPlanet() {
+        return distanceFromPreviousPlanet;
+    }
 
-        public double getDistanceFromSun() {
-            return distanceFromSun;
-        }
+    public int getRadius() {
+        return radius;
+    }
 
-        public double getRadius() {
-            return radius;
-        }
+    public SolarSystem getPreviousPlanet() {
+        return previousPlanet;
+    }
 
-        public SolarSystem getPreviousPlanet() {
-            return previousPlanet;
-        }
+    public SolarSystem getNextPlanet() {
+        return nextPlanet;
+    }
+}
 
-        public SolarSystem getNextPlanet() {
-            return nextPlanet;
-        }
-
-        private double calculateDistanceFromSun() {
-            if (previousPlanet != null) {
-                return previousPlanet.getDistanceFromSun() + distanceFromPrevious;
-            } else {
-                return 0;
-            }
-        }
-
-        private SolarSystem getNextPlanet(int ordinal) {
-            SolarSystem[] planets = SolarSystem.values();
-            if (ordinal < planets.length - 1) {
-                return planets[ordinal + 1];
-            } else {
-                return null;
-            }
-        }
+public class Main {
+    public static void main(String[] args) {
+        SolarSystem mercury = SolarSystem.MERCURY;
+        System.out.println("Planet: Mercury");
+        System.out.println("Distance from Sun: " + mercury.getDistanceFromSun() + " km");
+        System.out.println("Distance from Previous Planet: " + mercury.getDistanceFromPreviousPlanet() + " km");
+        System.out.println("Radius: " + mercury.getRadius() + " km");
+        System.out.println("Previous Planet: " + (mercury.getPreviousPlanet() != null ? mercury.getPreviousPlanet().name() : "None"));
+        System.out.println("Next Planet: " + (mercury.getNextPlanet() != null ? mercury.getNextPlanet().name() : "None"));
     }
 }
